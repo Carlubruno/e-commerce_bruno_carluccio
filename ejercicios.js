2197eef9ba8a489822c3f0fd2d053c46e6c0c7dd
bruno <bruno@gmail.com>
2024-09-05 22:16:44 -0300

    clase8

---
+++ ejercicios.js
@@ -0,0 +1,66 @@
+//     const card = document.querySelector("#card_title");
+//     card.innerText = "El arte de la guerra - Sun Tzu";
+
+// // //precio
+
+
+    // let title;
+    // let price;
+    // const boton = document.querySelector("#button");
+    // const card3 = document.querySelector("#card_title");
+    // const pricecard = document.querySelector("#price");
+
+    // boton.addEventListener("click", function(){
+    // title = prompt("Titulo");    
+    //     card3.innerText = title;
+    //     price = prompt("valor");
+    //     pricecard.innerText = "$" + price;
+        
+        
+
+    // let array = [];
+    // const h1 = document.querySelector("#products");
+    // h1.innerText = "Productos";
+
+    // for(let i = 1; i < 3; i++){
+    // array.push(`<div class="card"> <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/1.jpg" alt="" width="130px"> </div>`);
+    // document.querySelector(".countainer").innerHTML = (array);
+    // }
+    
+
+// let input1 = parseInt(prompt("ingrese un numero: "));
+// let inputOperator = parseInt(prompt("ingrese un operador: 1 = + | 2 = - | 3 = /"));
+// let input2 = parseInt(prompt("ingrese otro numero: "));
+// let resultSuma = (input1 + input2);
+// let resultResta = input1 - input2;
+// let resultDivision = input1 / input2;
+// if(true){
+// if(inputOperator == 1){
+//     alert("su suma " + resultSuma);
+// }
+// else if(inputOperator == 2){
+//     alert("su resta " + resultResta);
+// }
+// else if(inputOperator == 3){
+//     alert("su division:  " + resultDivision);
+// }
+// else{
+//     alert("seleccione un operador valido");
+// }
+// }
+
+
+// let number = parseInt(prompt("ingrese un numero entre 1 y 3!: " ));
+// const random = Math.floor(Math.random()*3);
+
+// if(number > 3 || number <= 0){
+//     alert("entre 1 y 3 flaquito, o queres que te lo repita dos veces");
+
+    
+//     }else if(number != random){
+//         alert("El numero no es igual, sigue intentando");
+//     }
+//     else{
+//         alert("Acertaste!");
+//     }
+
--- javaScript.js
+++ javaScript.js
@@ -1,8 +1,14 @@
-const card = document.querySelector(".card_title");
-card.innerText = "El arte de la guerra -Sun Tzu";
 
-//precio
 
-const price = document.querySelector(".card_price");
-price.innerText = "$4500";
 
+    let array = [];
+    const h1 = document.querySelector("#products");
+    h1.innerText = "Productos";
+
+    for(let i = 1; i < 6; i++){
+    array.push(`<div  class="card"> <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/1.jpg" alt="" width="130px">
+        <h1 class="title" id="#card_title">BMW</h1>
+        <p class="price" id="price">$687.000</p>
+        </div>  `);
+    document.querySelector(".countainer").innerHTML = (array);
+    }
