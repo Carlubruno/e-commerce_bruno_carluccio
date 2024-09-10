291983b55ad955130819afb6ce4dc4811de386a2
bruno <bruno@gmail.com>
2024-09-10 20:40:05 -0300

    tarea9

--- javaScript.js
+++ javaScript.js
@@ -2,21 +2,34 @@
 
 
     let array = [];
-    const h1 = document.querySelector("#products");
+    const h1 = document.querySelector("h1");
     h1.innerText = "Productos";
 
-    for(let i = 1; i < 2; i++){
-    array.push(`<div  class="card"> <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/1.jpg" alt="" width="170px">
-        <h1 class="title" id="#card_title">BMW</h1>
-        <p class="price" id="price">$687.000</p>
+    for(let i = 1; i < 10; i++){
+        if(i == 1){
+            title = "BMW M5"
+            price = 500
+        } else if (i == 2){
+            title = "Gaston Martin"
+            price = 800
+        } else if (i == 3){
+            title = "Mercedes AMG"
+            price = 900
+        } else if (i == 4){
+            title = "Audi a5"
+            price = 300
+        } else{
+            title = "Autito viejo"
+            price = 1000
+        } 
+    
+    
+    array.push(`<div  class="card"> <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" alt="" width="170px">
+        <h1 class="title" id="#card_title">${title}</h1>
+        <p class="price" id="price">${price}</p>
         </div>  `);
-    array.push(`<div  class="card"> <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/2.jpg" alt="" width="150px">
-        <h1 class="title" id="#card_title">Alfa Romeo</h1>
-        <p class="price" id="price">$850.000</p>
-        </div>  `);
-    array.push(`<div  class="card"> <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/3.jpg" alt="" width="150px">
-            <h1 class="title" id="#card_title">Mercedes AMG</h1>
-            <p class="price" id="price">$500.000</p>
-            </div>  `);
-    document.querySelector(".countainer").innerHTML = array;
+        
+
+
+    document.querySelector(".countainer").innerHTML = array.join().replaceAll(",", "");;
     }
