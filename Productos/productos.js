a75fca2c2a0b45242dd82ffaf366a82639c88d06
bruno <bruno@gmail.com>
2024-10-02 23:18:45 -0300

    rediseño

--- Productos/productos.js
+++ Productos/productos.js
@@ -1,11 +1,37 @@
-    let h1 = document.querySelector("h1");
-    h1.classList.add("product");
-    h1.innerText = "Productos";
+// class Producto {
+//     constructor(logo, titulo, detalle, precio, stock, imagen) {
+//         this.logo = logo;
+//         this.titulo = titulo;
+//         this.detalle = detalle;
+//         this.precio = precio;
+//         this.stock = stock;
+//         this.imagen = imagen;
+//     }
+// }
+
+// const producto = new Producto(
+//     "https://static.vecteezy.com/system/resources/previews/019/956/127/non_2x/bmw-transparent-bmw-free-free-png.png",
+//     "BMW Serie 3",
+//     "El BMW Serie 3 es una berlina media premium del segmento D, 5 plazas y tracción trasera o total fabricada por BMW. Cuenta con motores diésel y gasolina, con potencias comprendidas entre 120 y 320 CV, si bien también existe una versión deportiva denominada BMW M3 con 431 CV.  Se fabrica en la planta  Munich, Alemania, pero también en las plantas que BMW tiene en Tiexi, China, y México, concretamente en la factoría de San Luis Potosí donde se trasladó parte de la producción en 2018.", "Valor: $50.000",
+//     "Un. Disponible/s: 3", "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/1.jpg"
+// );
+
+// const producto2 = new Producto(
+//     "",
+//     "Chevrolet 1955",
+//     "El Chevrolet 1955 es símbolo de la era dorada de los autos estadounidenses, apreciado tanto por su ingeniería como por su estilo atemporal, lo que lo hace ideal para una tienda que ofrezca productos relacionados con autos clásicos o la cultura vintage.",
+//     "Valor: $100.000",
+//     "Un. Disponible/s: 1", 
+//     "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/7.jpg"
+// )
+    
+
+
     const data = [{
         id: 1,
         title: "BMW Serie 3",
         detail: "Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.\n\nIn hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.",
-        img: "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/1.jpg",
+        img: "http://dummyimage.com/450x300.png/5fa2dd/ffffff",
         price: 93207,
         Stock: 2,
         car_condition: "new"
@@ -13,7 +39,7 @@
         id: 2,
         title: "Aston Martin",
         detail: "Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.\n\nCurabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.",
-        img: "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/2.jpg",
+        img: "http://dummyimage.com/450x300.png/5fa2dd/ffffff",
         price: 250000,
         Stock: 1,
         car_condition: "new"
@@ -21,7 +47,7 @@
         id: 3,
         title: "Mercedes AMG",
         detail: "Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.\n\nVestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.",
-        img: "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/3.jpg",
+        img: "http://dummyimage.com/450x300.png/5fa2dd/ffffff",
         price: 95000,
         Stock: 2,
         car_condition: "new"
@@ -29,7 +55,7 @@
         id: 4,
         title: "Audio a5",
         detail: "Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.\n\nIn congue. Etiam justo. Etiam pretium iaculis justo.",
-        img: "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/4.jpg",
+        img: "http://dummyimage.com/450x300.png/dddddd/000000",
         price: 72000,
         Stock: 4,
         car_condition: "new"
@@ -37,7 +63,7 @@
         id: 5,
         title: "Ferrari",
         detail: "Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.\n\nProin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.",
-        img: "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/5.jpg",
+        img: "http://dummyimage.com/450x300.png/5fa2dd/ffffff",
         price: 95454,
         Stock: 1,
         car_condition: "new"
@@ -45,7 +71,7 @@
         id: 6,
         title: "Lamborghini",
         detail: "Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.\n\nCurabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.",
-        img: "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/6.jpg",
+        img: "http://dummyimage.com/450x300.png/dddddd/000000",
         price: 81812,
         Stock: 5,
         car_condition: "new"
@@ -53,7 +79,7 @@
         id: 7,
         title: "Chevrolet",
         "detail": "Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.\n\nMaecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.\n\nNullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.",
-        img: "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/7.jpg",
+        "img": "http://dummyimage.com/450x300.png/cc0000/ffffff",
         "price": 50000,
         "Stock": 1,
         "car_condition": "Used"
@@ -61,7 +87,7 @@
         id: 8,
         title: "R-Class",
         detail: "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.",
-        img: "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/8.jpg",
+        img: "http://dummyimage.com/450x300.png/5fa2dd/ffffff",
         price: 93662,
         Stock: 5,
         car_condition: "new"
@@ -69,21 +95,28 @@
         id: 9,
         title: "Land Cruiser",
         detail: "Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.\n\nCurabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.",
-        img: "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/9.jpg",
+        img: "http://dummyimage.com/450x300.png/ff4444/ffffff",
         price: 89470,
         Stock: 8,
         car_condition: "new"
     }]
+
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const productoId = urlParams.get('id');
+
     
-    
-    
-    let cards = data.map((productos) => 
-    `<div  class="card"> <img src="${productos.img}" alt="" width="170px">
-        <h1 class="title" id="card_title">${productos.title}</h1>
-        <p class="detail" id="detail">${productos.detail}</p>
-        <p class="price" id="price">$${productos.price}</p>
-        <p class="stock" id="stock">${productos.Stock}Un.</p>
-        <p class="condition" id="condition">${productos.car_condition}</p>
-        <a href="/Productos/productos.html?prod=${productos.id}" ><button class="btn" type="button">Ver mas</button></a></div> `
-    );
-        document.querySelector(".container").innerHTML = cards.join().replaceAll(",", "");
+
+
+
+    let cards = data.filter((productos => productos.id == ids)
+    `<div class="contenido">
+        <img class="logo" src="${productos.id}" >
+        <h1 class="title">${productos.title}</h1>
+        <img class="image" src="${productos.img}" >
+        <p class="detail">${productos.detail}</p>
+        <p>${productos.price}</p>
+        <p>${productos.Stock}</p>
+    </div> `)
+    document.querySelector("main").innerHTML = cards;
+
--- ejercicios.js
+++ ejercicios.js
@@ -111,3 +111,56 @@
 
     // const estacionFinal = estacion1();
     // alert(estacionFinal);
+
+
+    // function stock(){
+    //     let array = [];
+        
+    
+    //     for(let i = 1; i < 10; i++){
+    //         btn = "Ver más";
+    //         if(i == 1){
+    //             title = "BMW Serie 3"
+    //             price = "$50.000"
+    //         } else if (i == 2){
+    //             title = "Gaston Martin"
+    //             price = "$800.000"
+    //         } else if (i == 3){
+    //             title = "Mercedes AMG"
+    //             price = "$900.000"
+    //         } else if (i == 4){
+    //             title = "Audi a5"
+    //             price = "$300.000"
+    //         } else if (i == 5){
+    //             title = "Ferrari"
+    //             price = "$1.000.000"
+    //         } else if (i == 6){
+    //             title = "Lamborghini"
+    //             price = "$320.000"
+    //         } else if (i == 7){
+    //             title = "Chrysler"
+    //             price = "$100.000"
+    //         } else if (i == 8){
+    //             title = "Mustang"
+    //             price = "$360.000"
+    //         }  else{
+    //             title = "Masserati"
+    //             price = "$700.000"
+    //         }
+        
+    
+            
+    //     array.push(`<div  class="card"> <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" alt="" width="170px">
+    //         <h1 class="title" id="#card_title">${title}</h1>
+    //         <p class="price" id="price">${price}</p>
+    //         <a href=/Productos/productos.html ><button class="btn" type="button">${btn}</button></a>
+    //         </div> `);
+    //     }
+        
+        
+    //         document.querySelector(".countainer").innerHTML = array.join().replaceAll(",", "");
+    //     }
+        
+    
+    //     stock();
+    
